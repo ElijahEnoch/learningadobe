aba60807ecbcc1ae625e99db45ec403471b256e9
Elijah Gwak <69044758+ElijahEnoch@users.noreply.github.com>
2022-07-12 09:26:52 +0900

    XD tutorial

---
+++ XD Tutorial/📍Icon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="📍icon">
+        <div class="📍icon__boundary"></div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.📍icon {
+    &__boundary {
+        width: 24px;
+    }
+}
+</style>
